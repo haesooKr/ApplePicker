7854c8c8923183ec7104ea2a77fd7ea249e9f0d8
haesooKr <59234874+haesooKr@users.noreply.github.com>
2020-02-03 22:16:39 -0500

    Add HighScore

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -12,10 +12,24 @@ public class HighScore : MonoBehaviour
         
     }
 
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
     // Update is called once per frame
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
